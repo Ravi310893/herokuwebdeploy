88c999e3c2aed6c899eeae36728fa4bebe3c0eba
ravi <ravi310893@gmail.com>
2022-05-13 14:42:43 +0530

    added port and script

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Contact from './copmonents/contact/Contact'
 import Portfolio from './copmonents/portfolio/Portfolio'
 import Testimonials from './copmonents/testimonials/Testimonials'
 import Footer from './copmonents/footer/Footer'
+const port = process.env.port || 8000
 
 const App = () => {
   return (
